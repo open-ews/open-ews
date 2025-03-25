401cd84e770d30e3d8715376dbfa08741bb7d895
David Wilkie <dwilkie@gmail.com>
2025-03-25 21:50:07 +0700

    Add open-ews SNI to internal load balancer (#1806)

--- infrastructure/core/lb.tf
+++ infrastructure/core/lb.tf
@@ -1,4 +1,9 @@
-resource "aws_lb_listener_certificate" "this" {
+resource "aws_lb_listener_certificate" "public" {
   listener_arn    = local.region.public_load_balancer.https_listener.arn
+  certificate_arn = module.ssl_certificate.this.arn
+}
+
+resource "aws_lb_listener_certificate" "internal" {
+  listener_arn    = local.region.internal_load_balancer.https_listener.arn
   certificate_arn = module.ssl_certificate.this.arn
 }
