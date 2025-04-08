c6c6ee50ae4a3018c1868ec8c08828af5f6e8c07
Samnang Chhun <samnang.chhun@gmail.com>
2025-04-08 17:34:04 +0700

    Handle different operator selection and switching value input based on operator

--- app/javascript/controllers/beneficiary_filter_field_controller.js
+++ app/javascript/controllers/beneficiary_filter_field_controller.js
@@ -1,22 +1,51 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["toggleElement", "fieldName", "operator", "value"]
+  static targets = [
+    "toggleElement",
+    "fieldName",
+    "operator",
+    "value",
+    "isNullValue",
+  ]
 
   connect() {
-    this.toggle()
+    const enabled = this.toggleElementTarget.checked
+
+    if (!enabled) {
+      this.toggle()
+    }
   }
 
   toggle() {
     const enabled = this.toggleElementTarget.checked
 
     this.fieldNameTarget.disabled = !enabled
+
     this.operatorTarget.disabled = !enabled
     this.operatorTarget.value = null
 
+    this.#toggleValueInput()
+  }
+
+  operatorChanged() {
+    this.#toggleValueInput()
+  }
+
+  #toggleValueInput() {
+    const isNullSelected = this.operatorTarget.value === "is_null"
+
+    this.isNullValueTarget.disabled = !isNullSelected
+    this.isNullValueTarget.value = null
+    this.isNullValueTarget.style.display = isNullSelected ? "unset" : "none"
+
     // NOTE: handle multiple value inputs (eg. date select)
     this.valueTargets.forEach((e) => {
-      e.disabled = !enabled
+      e.disabled = !this.toggleElementTarget.checked || isNullSelected
+      // NOTE: hide the entire wrapper
+      e.closest(".value-input").style.display = isNullSelected
+        ? "none"
+        : "unset"
       e.value = null
     })
   }
