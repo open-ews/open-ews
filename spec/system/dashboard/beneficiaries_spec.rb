6b0ddec411e5ad396f3e75b06533ac5d69dfa8f3
Samnang Chhun <samnang.chhun@gmail.com>
2025-04-29 17:16:30 +0700

    Show beneficiary's addresses and fix tests

--- spec/system/dashboard/beneficiaries_spec.rb
+++ spec/system/dashboard/beneficiaries_spec.rb
@@ -1,6 +1,18 @@
 require "rails_helper"
 
 RSpec.describe "Beneficiaries" do
+  it "can list all beneficiaries" do
+    user = create(:user)
+    _beneficiary = create(:beneficiary, phone_number: "85516789111", account: user.account)
+    _other_beneficiary = create(:beneficiary, phone_number: "85510555123")
+
+    sign_in(user)
+    visit dashboard_beneficiaries_path
+
+    expect(page).to have_content("85516789111")
+    expect(page).not_to have_content("85510555123")
+  end
+
   it "creates a new beneficiary", :js do
     user = create(:user)
 
@@ -27,9 +39,7 @@ RSpec.describe "Beneficiaries" do
     create(:beneficiary, phone_number: "85516789111", account: user.account)
 
     sign_in(user)
-    visit dashboard_beneficiaries_path
-    click_on "New"
-
+    visit new_dashboard_beneficiary_path
 
     fill_in "Phone number", with: "85516789111"
     select "Cambodia", from: "Country"
@@ -37,32 +47,6 @@ RSpec.describe "Beneficiaries" do
     click_on "Create Beneficiary"
 
     expect(page).to have_text("Phone number has already been taken")
-  end
-
-  it "can list all beneficiaries" do
-    user = create(:user)
-    beneficiary = create(:beneficiary, account: user.account)
-    other_beneficiary = create(:beneficiary)
-
-    sign_in(user)
-    visit dashboard_beneficiaries_path
-
-    expect(page).to have_title("Contacts")
-
-    # TODO: Re-enable this once after data migration to native columns
-    # within("#page_actions") do
-    #   expect(page).to have_link("New", href: new_dashboard_beneficiary_path)
-    # end
-
-    within("#resources") do
-      expect(page).to have_content_tag_for(beneficiary)
-      expect(page).not_to have_content_tag_for(other_beneficiary)
-      expect(page).to have_content("#")
-      expect(page).to have_link(
-        beneficiary.id.to_s,
-        href: dashboard_beneficiary_path(beneficiary)
-      )
-    end
   end
 
   it "can delete a beneficiary" do
@@ -74,33 +58,35 @@ RSpec.describe "Beneficiaries" do
 
     click_on "Delete"
 
-    expect(page).to have_current_path(dashboard_beneficiaries_path, ignore_query: true)
-    expect(page).to have_text("Contact was successfully destroyed.")
+    expect(page).to have_text("Beneficiary was successfully destroyed.")
   end
 
-  it "can show a beneficiary" do
+  it "can edit a beneficiary", :js do
     user = create(:user)
-    phone_number = generate(:phone_number)
     beneficiary = create(
       :beneficiary,
+      gender: "M",
       account: user.account,
-      phone_number:,
-      metadata: { "location" => { "country" => "Cambodia" } }
+      phone_number: "85516789111",
     )
 
     sign_in(user)
     visit dashboard_beneficiary_path(beneficiary)
+    click_on "Edit"
 
-    expect(page).to have_title("Contact #{beneficiary.id}")
+    fill_in "Phone number", with: "85516789111"
+    select "Female", from: "Gender"
+    fill_in "Language code", with: "km"
 
-    # TODO: Re-enable this once after data migration to native columns
-    # within("#page_actions") do
-    #   expect(page).to have_link("Edit", href: edit_dashboard_beneficiary_path(beneficiary))
-    # end
+    click_on "Add Address"
+    fill_in "ISO region code", with: "KH-12"
 
-    within(".beneficiary") do
-      expect(page).to have_content(beneficiary.id)
-      expect(page).to have_content("Cambodia")
-    end
+    click_on "Save"
+
+    expect(page).to have_content("Beneficiary was successfully updated.")
+    expect(page).to have_content("85516789111")
+    expect(page).to have_content("Female")
+    expect(page).to have_content("km")
+    expect(page).to have_content("KH-12")
   end
 end
