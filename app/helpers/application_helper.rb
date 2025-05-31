da0cb48a775e10254feed0ec072da3aaed67e6ad
David Wilkie <dwilkie@gmail.com>
2025-05-31 14:49:56 +0700

    WIP

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,17 +19,12 @@ module ApplicationHelper
     end
   end
 
-  def page_title(title:, subtitle: nil, &block)
+  def page_title(title:, &block)
     content_for(:page_title, title)
 
     content_tag(:div, class: "card-header d-flex justify-content-between align-items-center") do
       content = "".html_safe
       content += content_tag(:span, title, class: "h2")
-
-      if subtitle.present?
-        content += " "
-        content += content_tag(:small, subtitle)
-      end
 
       if block_given?
         content += content_tag(:div, id: "page_actions", class: "card-header-actions") do
