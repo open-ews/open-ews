e2ddab741a0cb82cf8517d621623f1bf4a1fb84e
David Wilkie <dwilkie@gmail.com>
2025-06-05 23:02:33 +0700

    Handle account settings validations

--- infrastructure/modules/app/s3.tf
+++ infrastructure/modules/app/s3.tf
@@ -23,7 +23,7 @@ resource "aws_s3_bucket_cors_configuration" "uploads" {
   cors_rule {
     allowed_headers = ["*"]
     allowed_methods = ["GET", "POST", "PUT"]
-    allowed_origins = ["https://*.open-ews.org"]
+    allowed_origins = ["https://*.open-ews.org", "https://scfm.somleng.org"]
     max_age_seconds = 3000
   }
 }
