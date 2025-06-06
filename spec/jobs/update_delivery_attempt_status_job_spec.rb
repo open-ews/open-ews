5f873c1c131b41ef79b4e674bb0ab35f4a292f2b
David Wilkie <dwilkie@gmail.com>
2025-06-06 20:40:48 +0700

    Handle duplicate delivery attempt updates (#1937)

--- spec/jobs/update_delivery_attempt_status_job_spec.rb
+++ spec/jobs/update_delivery_attempt_status_job_spec.rb
@@ -1,37 +1,35 @@
 require "rails_helper"
 
 RSpec.describe UpdateDeliveryAttemptStatusJob do
-  describe "#perform" do
-    it "updates the status of a delivery attempt" do
-      account = create(:account, somleng_account_sid: "account-sid", somleng_auth_token: "auth-token")
-      broadcast = create(:broadcast, :running, account:)
-      notification = create(:notification, broadcast:)
-      delivery_attempt = create(
-        :delivery_attempt,
-        :initiated,
-        notification:,
-        metadata: {
-          "somleng_call_sid" => "call-sid"
-        }
-      )
-      stub_somleng_request(response: { body: { "status" => "completed", "duration" => "87" }.to_json })
+  it "updates the status of a delivery attempt" do
+    account = create(:account)
+    broadcast = create(:broadcast, :running, account:)
+    notification = create(:notification, broadcast:)
+    delivery_attempt = create(
+      :delivery_attempt,
+      :initiated,
+      notification:,
+      metadata: {
+        "somleng_call_sid" => "call-sid"
+      }
+    )
+    stub_somleng_request(response: { body: { "status" => "completed", "duration" => "87" }.to_json })
 
-      UpdateDeliveryAttemptStatusJob.perform_now(delivery_attempt)
+    UpdateDeliveryAttemptStatusJob.perform_now(delivery_attempt)
 
-      expect(delivery_attempt).to have_attributes(
-        metadata: {
-          "somleng_call_sid" => "call-sid",
-          "somleng_status" => "completed",
-          "call_duration" => 87
-        },
-        status: "succeeded",
-        completed_at: be_present,
-        status_update_queued_at: nil
-      )
-    end
+    expect(delivery_attempt).to have_attributes(
+      metadata: {
+        "somleng_call_sid" => "call-sid",
+        "somleng_status" => "completed",
+        "call_duration" => 87
+      },
+      status: "succeeded",
+      completed_at: be_present,
+      status_update_queued_at: nil
+    )
+  end
 
-    def stub_somleng_request(response:)
-      stub_request(:get, %r{https://api.somleng.org}).to_return(response)
-    end
+  def stub_somleng_request(response:)
+    stub_request(:get, %r{https://api.somleng.org}).to_return(response)
   end
 end
