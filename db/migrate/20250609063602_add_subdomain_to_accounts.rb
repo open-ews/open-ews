6238860b6cf8abc1de3c3888191bfeb858a8b31d
David Wilkie <dwilkie@gmail.com>
2025-06-10 13:31:55 +0700

    Cleanup subdomains PR

--- db/migrate/20250609063602_add_subdomain_to_accounts.rb
+++ db/migrate/20250609063602_add_subdomain_to_accounts.rb
@@ -1,16 +1,6 @@
 class AddSubdomainToAccounts < ActiveRecord::Migration[8.0]
   def change
-    add_column :accounts, :subdomain, :citext
-
-    reversible do |dir|
-      dir.up do
-        Account.find_each do |account|
-          account.update_columns(subdomain: account.name.parameterize)
-        end
-      end
-    end
-
-    change_column_null :accounts, :subdomain, false
+    add_column :accounts, :subdomain, :citext, null: false
     add_index :accounts, :subdomain, unique: true
   end
 end
