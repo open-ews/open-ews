b19d762d48bae433deb91d0e51a629a09e304bf9
David Wilkie <dwilkie@gmail.com>
2025-06-12 21:43:43 +0700

    Fix AccountForm when uploading logo

--- spec/forms/account_form_spec.rb
+++ spec/forms/account_form_spec.rb
@@ -2,11 +2,12 @@ require "rails_helper"
 
 RSpec.describe AccountForm do
   it "saves the form" do
-    account = create(:account, subdomain: "my-account")
+    account = create(:account, :with_logo, subdomain: "my-account")
     form = AccountForm.new(
       object: account,
       name: "My account",
       country: "KH",
+      logo: nil,
       somleng_account_sid: "",
       somleng_auth_token: "",
       notification_phone_number: "",
@@ -18,6 +19,7 @@ RSpec.describe AccountForm do
     expect(account).to have_attributes(
       name: "My account",
       iso_country_code: "KH",
+      logo: be_attached,
       somleng_account_sid: nil,
       somleng_auth_token: nil,
       notification_phone_number: nil,
