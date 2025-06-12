3616d3ddcf3f93e7493a708e98a90af4dd590887
David Wilkie <dwilkie@gmail.com>
2025-06-12 20:02:06 +0700

    WIP

--- lib/simple_form_components/input_group.rb
+++ lib/simple_form_components/input_group.rb
@@ -1,10 +1,6 @@
 # custom component requires input group wrapper
 module SimpleFormComponents
   module InputGroup
-    def prepend(_wrapper_options = nil)
-      template.content_tag(:span, options[:prepend], class: "input-group-text")
-    end
-
     def append(_wrapper_options = nil)
       template.content_tag(:span, options[:append], class: "input-group-text")
     end
