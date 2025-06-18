27ea471b17c618e179849e782f2b3ea211a4f912
David Wilkie <dwilkie@gmail.com>
2025-06-18 13:38:04 +0700

    Configure open-ews.org mailer

---
+++ infrastructure/core/ses.tf
@@ -0,0 +1,60 @@
+resource "aws_ses_domain_identity" "domain" {
+  domain = aws_route53_zone.this.name
+}
+
+resource "aws_ses_domain_mail_from" "mail_from" {
+  domain           = aws_ses_domain_identity.domain.domain
+  mail_from_domain = "mail.${aws_ses_domain_identity.domain.domain}"
+}
+
+resource "aws_ses_domain_identity_verification" "verification" {
+  domain     = aws_ses_domain_identity.domain.id
+  depends_on = [aws_route53_record.amazonses_verification_record]
+}
+
+resource "aws_ses_domain_dkim" "dkim" {
+  domain = aws_ses_domain_identity.domain.domain
+}
+
+resource "aws_route53_record" "dkim_amazonses_verification_record" {
+  count   = 3
+  zone_id = aws_route53_zone.this.zone_id
+  name    = "${element(aws_ses_domain_dkim.dkim.dkim_tokens, count.index)}._domainkey.${aws_route53_zone.this.name}"
+  type    = "CNAME"
+  ttl     = "600"
+  records = ["${element(aws_ses_domain_dkim.dkim.dkim_tokens, count.index)}.dkim.amazonses.com"]
+}
+
+resource "aws_route53_record" "ses_domain_mail_from_mx" {
+  zone_id = aws_route53_zone.this.zone_id
+  name    = aws_ses_domain_mail_from.mail_from.mail_from_domain
+  type    = "MX"
+  ttl     = "600"
+  records = ["10 feedback-smtp.us-east-1.amazonses.com"]
+}
+
+resource "aws_route53_record" "spf_ses_domain_mail_from_txt" {
+  zone_id = aws_route53_zone.this.zone_id
+  name    = aws_ses_domain_mail_from.mail_from.mail_from_domain
+  type    = "TXT"
+  ttl     = "600"
+  records = ["v=spf1 include:amazonses.com -all"]
+}
+
+resource "aws_route53_record" "amazonses_verification_record" {
+  zone_id = aws_route53_zone.this.zone_id
+  name    = "_amazonses"
+  type    = "TXT"
+  ttl     = "600"
+  records = [
+    aws_ses_domain_identity.domain.verification_token
+  ]
+}
+
+resource "aws_route53_record" "dmarc_record" {
+  zone_id = aws_route53_zone.this.zone_id
+  name    = "_dmarc"
+  type    = "TXT"
+  ttl     = "600"
+  records = ["v=DMARC1;p=none"]
+}
