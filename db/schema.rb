7bb9a0cb6c9dbab96d82b13b86ac2f5c66f2cd9c
David Wilkie <dwilkie@gmail.com>
2025-06-24 19:51:10 +0700

    Fix slow queries related to dashboard home (#1983)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_17_075644) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_24_124359) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "pg_catalog.plpgsql"
@@ -237,6 +237,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_17_075644) do
     t.index ["broadcast_id"], name: "index_notifications_on_broadcast_id"
     t.index ["completed_at"], name: "index_notifications_on_completed_at"
     t.index ["priority"], name: "index_notifications_on_priority"
+    t.index ["status", "created_at", "broadcast_id"], name: "index_notifications_on_status_and_created_at_and_broadcast_id"
     t.index ["status"], name: "index_notifications_on_status"
   end
 
