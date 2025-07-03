f4277f43e3ed3f8679f4974d35d114e001458a39
Samnang Chhun <samnang.chhun@gmail.com>
2025-07-03 16:14:58 +0700

    Deny access to cross account subdomain (#2004)

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -6,10 +6,22 @@ class DashboardController < ApplicationController
   layout "dashboard"
 
   before_action :authenticate_user!, :set_locale
+  before_action :authorize_account!
 
   helper_method :current_account
 
   private
+
+  def authorize_account!
+    return if current_account == current_user.account
+
+    sign_out(current_user)
+    redirect_to(
+      new_user_session_url(host: current_account.subdomain_host),
+      status: :see_other,
+      alert: t("devise.failure.invalid", authentication_keys: :email)
+    )
+  end
 
   def paginate_resources(resources_scope)
     resources_scope.latest_first.page(params[:page]).without_count
@@ -17,10 +29,6 @@ class DashboardController < ApplicationController
 
   def set_locale
     I18n.locale = current_user.language
-  end
-
-  def current_account
-    current_user.account
   end
 
   def filter_param
