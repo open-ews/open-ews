f8f297ec8ec13dd7efc02dbde523cf3d96c75873
David Wilkie <dwilkie@gmail.com>
2025-09-04 15:44:24 +0700

    Display status preview for pending broadcasts (#2083)

--- app/decorators/broadcast_decorator.rb
+++ app/decorators/broadcast_decorator.rb
@@ -1,9 +1,11 @@
 class BroadcastDecorator < ApplicationDecorator
   ORDERED_STATUSES = %i[failed succeeded pending].freeze
 
+  delegate :notifications, to: :object, private: true
+
   def notification_stats
     @notification_stats ||= begin
-      stats = object.notifications.group(:status).count.symbolize_keys
+      stats = generate_stats
       ORDERED_STATUSES.each_with_object({}) { |s, result| result[s] = stats.fetch(s, 0) }
     end
   end
@@ -17,6 +19,40 @@ class BroadcastDecorator < ApplicationDecorator
   end
 
   def total_notifications_count
-    @total_notifications_count ||= object.notifications.count
+    @total_notifications_count ||= preview? ? approximate_notifications_count : notifications.count
+  end
+
+  def approximate_beneficiaries
+    @approximate_beneficiaries ||= ApplicationController.helpers.pluralize_model(
+      approximate_notifications_count,
+      Beneficiary.model_name,
+      formatter: ->(count) { ActiveSupport::NumberHelper.number_to_human(count) }
+    )
+  end
+
+  def state_machine
+    @state_machine ||= BroadcastStateMachine.new(object.status)
+  end
+
+  private
+
+  def generate_stats
+    preview? ? preview_stats : notifications.group(:status).count.symbolize_keys
+  end
+
+  def preview_stats
+    { pending: approximate_notifications_count }
+  end
+
+  def preview?
+    notifications.none?
+  end
+
+  def broadcast_preview
+    @broadcast_preview ||= BroadcastPreview.new(object)
+  end
+
+  def approximate_notifications_count
+    @approximate_notifications_count ||= broadcast_preview.beneficiaries.count
   end
 end
