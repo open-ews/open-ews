9ddf38884c15e9bb66bb389db5412bfd2c96c80b
David Wilkie <dwilkie@gmail.com>
2025-12-02 13:03:48 +0700

    WIP

--- config/application.rb
+++ config/application.rb
@@ -50,10 +50,10 @@ module OpenEWS
 
     config.exceptions_app = self.routes
 
-    # encryption_config = config.app_settings.fetch(:active_record_encryption)
-    # config.active_record.encryption.primary_key = encryption_config.fetch(:primary_key)
-    # config.active_record.encryption.deterministic_key = encryption_config.fetch(:deterministic_key)
-    # config.active_record.encryption.key_derivation_salt = encryption_config.fetch(:key_derivation_salt)
+    encryption_config = config.app_settings.fetch(:active_record_encryption)
+    config.active_record.encryption.primary_key = encryption_config.fetch(:primary_key)
+    config.active_record.encryption.deterministic_key = encryption_config.fetch(:deterministic_key)
+    config.active_record.encryption.key_derivation_salt = encryption_config.fetch(:key_derivation_salt)
   end
 end
 
