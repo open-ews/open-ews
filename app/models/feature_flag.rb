1e632b4fb21c72e179b27b34f9f6d4fb338f9ade
David Wilkie <dwilkie@gmail.com>
2026-05-27 18:11:46 +0700

    Add mock approval process (#2355)

--- app/models/feature_flag.rb
+++ app/models/feature_flag.rb
@@ -1,12 +1,17 @@
 class FeatureFlag
   PERMISSIONS = {
     1 => [ :alerts ],
+    2 => [ :alerts, :alert_approval ],
     1297 => [ :alerts ],
     1198 => [ :alerts ],
     1300 => [ :alerts ],
+    1301 => [ :alerts, :alert_approval ],
     1331 => [ :alerts ],
     1332 => [ :alerts ],
-    1495 => [ :alerts ]
+    1495 => [ :alerts ],
+    1693 => [ :alerts, :alert_approval ],
+    1694 => [ :alerts, :alert_approval ],
+    1695 => [ :alerts, :alert_approval ]
   }
 
   class << self
