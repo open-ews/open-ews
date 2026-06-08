73251f54da3d3206879aee80e59ce843aa98a908
Samnang Chhun <samnang.chhun@gmail.com>
2026-06-08 16:27:32 +0700

    WIP

--- spec/workflows/update_broadcast_spec.rb
+++ spec/workflows/update_broadcast_spec.rb
@@ -2,20 +2,14 @@ require "rails_helper"
 
 RSpec.describe UpdateBroadcast do
   it "updates the broadcast" do
-    broadcast = create(:broadcast, :pending)
+    broadcast = create(:broadcast, :running)
 
-    UpdateBroadcast.call(broadcast, desired_status: :queued)
+    UpdateBroadcast.call(broadcast, desired_status: :completed)
 
-    expect(broadcast).to have_attributes(status: "queued")
+    expect(broadcast).to have_attributes(status: "completed")
     expect(broadcast.account.events).to include(
       have_attributes(
         type: "broadcast.updated",
-        details: hash_including(
-          "data" => hash_including(
-            "id" => broadcast.id.to_s,
-            "type" => "broadcast"
-          )
-        )
       )
     )
   end
