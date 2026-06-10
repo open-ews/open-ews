c0035bd99bb26946141f3771e39ab0db4517654f
David Wilkie <dwilkie@gmail.com>
2026-06-10 20:22:26 +0700

    WIP

---
+++ spec/models/country_address_data/myanmar_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+module CountryAddressData
+  RSpec.describe Myanmar do
+    it "returns address localities in Myanmar" do
+      result = Myanmar.new.address_data
+
+      expect(result).to be_an(Array)
+      expect(result[0]).to be_a(CountryAddressData::Locality)
+      expect(result[0]).to have_attributes(
+        value: "MM-11",
+        name_en: "Kachin",
+        name_local: "ကချင်ပြည်နယ်"
+      )
+    end
+  end
+end
