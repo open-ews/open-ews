2f31ef33a51d2449e76a746fdfeacade480c6658
David Wilkie <dwilkie@gmail.com>
2026-08-10 20:50:19 +0700

    Fix n+1 on API controllers

--- app/controllers/api/v1/beneficiary_groups_controller.rb
+++ app/controllers/api/v1/beneficiary_groups_controller.rb
@@ -2,7 +2,7 @@ module API
   module V1
     class BeneficiaryGroupsController < APIController
       def index
-        apply_filters(scope.includes(include_parameter(only: :members)), with: BeneficiaryGroupFilter)
+        apply_filters(scope.includes(:members), with: BeneficiaryGroupFilter)
       end
 
       def show
