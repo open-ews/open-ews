669767464e4eca45016087c82f234673ba8802b2
David Wilkie <dwilkie@gmail.com>
2026-08-29 15:27:41 +0700

    WIP

--- app/models/country_address_data.rb
+++ app/models/country_address_data.rb
@@ -1,24 +1,22 @@
 module CountryAddressData
   Locality = Data.define(:value, :name_en, :name_local, :subdivisions)
-  Configuration = Data.define(:address_field, :address_data)
+  Configuration = Data.define(:local_language, :localities_data) do
+    def localities
+      localities_data.call
+    end
+  end
 
   SETTINGS = {
-    KH: Configuration.new(address_field: FieldDefinitions::BeneficiaryFields.find_by!(name: :administrative_division_level_3_code), address_data: -> { CountryAddressData::Cambodia.address_data }),
-    LA: Configuration.new(address_field: FieldDefinitions::BeneficiaryFields.find_by!(name: :administrative_division_level_2_code), address_data: -> { CountryAddressData::Laos.address_data }),
-    NP: Configuration.new(address_field: FieldDefinitions::BeneficiaryFields.find_by!(name: :administrative_division_level_2_code), address_data: -> { CountryAddressData::Nepal.address_data }),
-    MM: Configuration.new(address_field: FieldDefinitions::BeneficiaryFields.find_by!(name: :administrative_division_level_5_code), address_data: -> { CountryAddressData::Myanmar.address_data })
+    KH: Configuration.new(local_language: :km, localities_data: -> { CountryAddressData::Cambodia.address_data }),
+    LA: Configuration.new(local_language: :lo, localities_data: -> { CountryAddressData::Laos.address_data }),
+    NP: Configuration.new(local_language: :ne, localities_data: -> { CountryAddressData::Nepal.address_data }),
+    MM: Configuration.new(local_language: :my, localities_data: -> { CountryAddressData::Myanmar.address_data })
   }
-
-  def self.address_field(iso_country_code)
-    return nil unless supported?(iso_country_code)
-
-    SETTINGS.fetch(iso_country_code.to_sym).address_field
-  end
 
   def self.address_data(iso_country_code)
     return [] unless supported?(iso_country_code)
 
-    SETTINGS.fetch(iso_country_code.to_sym).address_data.call
+    SETTINGS.fetch(iso_country_code.to_sym)
   end
 
   def self.supported?(iso_country_code)
--- spec/models/country_address_data/myanmar_spec.rb
+++ spec/models/country_address_data/myanmar_spec.rb
@@ -5,12 +5,15 @@ module CountryAddressData
     it "returns address localities in Myanmar" do
       result = CountryAddressData.address_data(:MM)
 
-      expect(result).to be_an(Array)
-      expect(result[0]).to be_a(CountryAddressData::Locality)
-      expect(result[0]).to have_attributes(
-        value: "MM-11",
-        name_en: "Kachin",
-        name_local: "ကချင်ပြည်နယ်"
+      expect(result).to have_attributes(
+        local_language: :my,
+        localities: include(
+          have_attributes(
+            value: "MM-11",
+            name_en: "Kachin",
+            name_local: "ကချင်ပြည်နယ်"
+          )
+        )
       )
     end
   end
