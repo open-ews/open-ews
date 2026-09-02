fd5fed855079245bb044861f95b2234d92f0f709
David Wilkie <dwilkie@gmail.com>
2026-09-02 12:32:39 +0700

    Fix beneficiary filter edit (#2467)

--- app/models/beneficiary_filter_data.rb
+++ app/models/beneficiary_filter_data.rb
@@ -5,18 +5,7 @@ class BeneficiaryFilterData
   attribute :data, FilterDataType.new(field_definitions: FieldDefinitions::BeneficiaryFields)
   attribute :address_data_field_definition
 
-  def address_fields
-    @address_fields ||= data.fields.each_with_object({}) do |(_name, field), result|
-      next unless field.field_definition.prefix&.address?
-
-      result[field.name] = field
-    end
-  end
-
   def address_data_field
-    fields = address_fields.values
-    return unless fields.one?
-
-    fields.first if fields.first.name == address_data_field_definition.name
+    data.fields.values.find { it.field_definition == address_data_field_definition }
   end
 end
